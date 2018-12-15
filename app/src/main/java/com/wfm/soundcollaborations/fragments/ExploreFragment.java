2a854bdbcc60d5a19ab63f1e108af9c830eec0d7
WilhelmKrohmer <w.g.krohmer@gmail.com>
2018-12-15 19:52:35 +0100

    refactoring text strings

--- app/src/main/java/com/wfm/soundcollaborations/fragments/ExploreFragment.java
+++ app/src/main/java/com/wfm/soundcollaborations/fragments/ExploreFragment.java
@@ -45,7 +45,7 @@ public class ExploreFragment extends Fragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
     {
-        inflater.inflate(R.menu.exploration, menu);
+        inflater.inflate(R.menu.explore_activity_menu, menu);
         super.onCreateOptionsMenu(menu,inflater);
     }
 }
