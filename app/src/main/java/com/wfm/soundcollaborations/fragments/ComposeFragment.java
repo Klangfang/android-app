3f1041ea0017e41feb277515d6efab68368ba07e
WilhelmKrohmer <w.g.krohmer@gmail.com>
2019-03-05 22:38:18 +0100

    add recycler view to compose fragment

--- app/src/main/java/com/wfm/soundcollaborations/fragments/ComposeFragment.java
+++ app/src/main/java/com/wfm/soundcollaborations/fragments/ComposeFragment.java
@@ -1,42 +1,64 @@
 package com.wfm.soundcollaborations.fragments;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.wfm.soundcollaborations.R;
 import com.wfm.soundcollaborations.activities.MainActivity;
 
-/**
- * Created by Markus Eberts on 09.11.16.
- */
 public class ComposeFragment extends Fragment {
 
-    private TabLayout tlComposition;
-    private ViewPager vpComposition;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private View root;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_compose, container, false);
+        root = inflater.inflate(R.layout.fragment_compose, container, false);
         initToolbar();
-        return  root;
+        initRecyclerView();
+        return root;
     }
 
-    private void initToolbar()
-    {
+    /**
+     * This method initializes the top app bar and sets a custom
+     * title and background color.
+     * */
+    private void initToolbar() {
         MainActivity mainActivity = (MainActivity) getActivity();
         assert mainActivity != null;
         Toolbar toolbar = mainActivity.getToolbar();
         toolbar.setTitle(R.string.bnm_compose);
         toolbar.setBackgroundColor(getResources().getColor(R.color.navigation));
         toolbar.setTitleTextColor(getResources().getColor(R.color.toolbar_title));
+    }
+
+    /**
+     * This method initializes the recycler view that holds all public compositions
+     * */
+    private void initRecyclerView() {
+        recyclerView = root.findViewById(R.id.open_compositions_recycler_view);
+
+        // This improves performance of the recycler view, if the layout size is fixed
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter
+        // TODO create adapter.java file to manage the views that will be loaded into the recycler view
+        //mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
