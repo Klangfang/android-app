a4240e5a4afa0b5bc93275084c8fde93a498a258
WilhelmKrohmer <w.g.krohmer@gmail.com>
2019-04-28 15:29:55 +0200

    Add play button to composition layout

--- app/src/main/java/com/wfm/soundcollaborations/fragments/ComposeFragment.java
+++ app/src/main/java/com/wfm/soundcollaborations/fragments/ComposeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -13,9 +14,15 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.ohoussein.playpause.PlayPauseView;
 import com.wfm.soundcollaborations.Classes.Composition;
+import com.wfm.soundcollaborations.Editor.model.audio.AudioPlayer;
+import com.wfm.soundcollaborations.Editor.model.composition.CompositionBuilder;
+import com.wfm.soundcollaborations.Editor.utils.JSONUtils;
 import com.wfm.soundcollaborations.R;
 import com.wfm.soundcollaborations.activities.MainActivity;
 import com.wfm.soundcollaborations.adapter.CompositionAdapter;
@@ -23,19 +30,37 @@ import com.wfm.soundcollaborations.adapter.CompositionAdapter;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import butterknife.OnClick;
+
+/**
+ * The Compose Fragment provides public compositions that the user can join and an option
+ * to create a new composition.
+ *
+ * It loads and displays {@link Composition} objects.
+ * */
+
 public class ComposeFragment extends Fragment {
 
-    //private RecyclerView recyclerView;
-    //private RecyclerView.Adapter mAdapter;
-    //private RecyclerView.LayoutManager layoutManager;
     private View root;
+    private String compositionOverview;
+    private AudioPlayer audioPlayer;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_compose, container, false);
         initToolbar();
-        //initRecyclerView();
+
+        //JSON Audio Mock Data
+        String jsonData = "{"
+                + " 'uuid': '3423423-432434-43243241-33-22222',"
+                + " 'sounds': ["
+
+                + "   {'length': 30680, 'track': 2, 'start_position': 20000, 'link': "
+                + "'https://stereoninjamusic.weebly.com/uploads/4/5/7/5/45756923/the_midnight_ninja.ogg'}"
+
+                + " ]"
+                + "}";
 
         //New array with test data for composition views
         ArrayList<Composition> compositions = new ArrayList<>();
@@ -56,6 +81,7 @@ public class ComposeFragment extends Fragment {
     /**
      * This method initializes the top app bar and sets a custom
      * title and background color.
+     * TODO Use recycler view here instead of list view
      * */
     private void initToolbar() {
         MainActivity mainActivity = (MainActivity) getActivity();
@@ -67,27 +93,10 @@ public class ComposeFragment extends Fragment {
     }
 
 
-
-
-
-    /* TODO Use recycler view here instead of list view
-     * This method initializes the recycler view that holds all public compositions
-     * */
-    /*
-    private void initRecyclerView() {
-        recyclerView = root.findViewById(R.id.open_compositions_recycler_view);
-
-        // This improves performance of the recycler view, if the layout size is fixed
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter
-        // TODO create adapter.java file to manage the views that will be loaded into the recycler view
-        //mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
-    }*/
+    public void playComposition(View view)
+    {
+        Toast.makeText(getActivity(), "Play!", Toast.LENGTH_LONG).show();
+        ((PlayPauseView) view).toggle();
+    }
 
 }
