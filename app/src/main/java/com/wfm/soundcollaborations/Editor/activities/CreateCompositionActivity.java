0aa2ab3b609ebdb8a40d2dee5693746c2d5a1fc7
Franca Winter <franca@atelierdisko.de>
2019-08-02 17:15:17 +0200

    Add dummy event listener for menu

--- app/src/main/java/com/wfm/soundcollaborations/Editor/activities/CreateCompositionActivity.java
+++ app/src/main/java/com/wfm/soundcollaborations/Editor/activities/CreateCompositionActivity.java
@@ -7,12 +7,12 @@ import com.wfm.soundcollaborations.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
 
-
 public class CreateCompositionActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +37,21 @@ public class CreateCompositionActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        // Check which items are being clicked by checking ID
+        int itemId = item.getItemId();
+
+        // TODO: Go to empty editor activity instead. See startEditorActivity() below...
+        if (itemId == R.id.set_composition_title) {
+             Toast.makeText(this, "Geklickt!", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // TODO: still listens on deprecated button
+    // See https://developer.android.com/training/basics/firstapp/starting-activity#java
     private void startEditorActivity(View view) {
         Intent intent = new Intent(view.getContext(), EditorActivity.class);
         //intent.putExtra(PICK_RESPONSE, response);
